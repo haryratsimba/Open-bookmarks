0d47da3f8333c93b73099e22b18f6cb448ef99da
Hary <haryratsimba06@gmail.com>
2015-04-15 20:16:10 +0200

    Formatage du JavaScript

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 Ractive.DEBUG = false;
 
-var bookmarks = new Ractive({
-    el: $('#bookmark-extension'),
-    template: $('#bookmark-template').val(),
-    data: { bookmarks: [] }
-});
+var bookmarks = new Ractive( {
+	el: $( '#bookmark-extension' ),
+	template: $( '#bookmark-template' ).val(),
+	data: { bookmarks: [] }
+} );
 
 // Ouverture des bookmarks qui seront ajoutés lors d'une recherche, dans un nouvel onglet
-$('#bookmark-results').on('click', 'a.bookmark', function(e) {
-    e.preventDefault();
-    chrome.tabs.create({ url: $(this).attr('href') });
-});
+$( '#bookmark-results' ).on( 'click', 'a.bookmark', function( e ) {
+	e.preventDefault();
+	chrome.tabs.create( { url: $( this ).attr( 'href' ) } );
+} );
 
 // Lorsque la valeur de l'input change : https://gist.github.com/brandonaaskov/1596867
-$('#bookmark-input')
-.attr('autofocus', 'autofocus')
-.bind('input', function() {
-    chrome.runtime.sendMessage({ query: $(this).val() }, function(response) {
-        bookmarks.set('bookmarks', request.bookmarks);
-    });
-});
+$( '#bookmark-input' )
+	.attr( 'autofocus', 'autofocus' )
+	.bind( 'input', function() {
+		chrome.runtime.sendMessage( { query: $( this ).val() }, function( response ) {
+			bookmarks.set( 'bookmarks', request.bookmarks );
+		} );
+	} );
 
 // Permet de charger les bookmarks dans la popup
-window.refreshBookmarks = function(res) {
-    bookmarks.set('bookmarks', res);
+window.refreshBookmarks = function( res ) {
+	bookmarks.set( 'bookmarks', res );
 };
